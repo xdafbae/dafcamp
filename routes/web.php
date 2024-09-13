40370731fbee150f5d76603de2f516cb55c476c4
dafaax <109255243+dafaax@users.noreply.github.com>
2024-09-14 02:06:06 +0700

    instal and integrated socialite

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,6 +38,10 @@ Route::get('/success-checkout', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+//socialite routes
+Route::get('sign-in-google',[UserController::class, 'google'])->name('user.login.google');
+Route::get('auth/google/callback',[UserController::class, 'handleProviderCallback'])->name('user.google.callback');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
