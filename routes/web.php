f7bc270832e765bef72e9023f5c3c62782e9091f
dafaax <109255243+dafaax@users.noreply.github.com>
2024-09-21 00:34:50 +0700

    midtrans callback function

--- routes/web.php
+++ routes/web.php
@@ -48,6 +48,9 @@ Route::get('/login', function () {
 Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
 Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
 
+//midtrans route
+Route::get('payment/success', [UserController::class, 'midtransCallback']);
+Route::post('payment/success', [UserController::class, 'midtransCallback']);
 
 
 Route::middleware('auth')->group(function () {
